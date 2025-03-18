982d3d58bff1dac438033673127e26d16e93bc3c
Viktor Sorokin <jabervik@gmail.com>
2025-03-18 12:31:53 +0300

    Add homework

--- homework.py
+++ homework.py
@@ -1,6 +1,5 @@
 from datetime import time
 
-
 def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
@@ -8,24 +7,33 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
+    if 22 <= current_time.hour or current_time.hour  < 6:
+        should_use_dark_theme = True
+    else:
+        should_use_dark_theme = False
+
+    is_dark_theme =should_use_dark_theme
+
     assert is_dark_theme is True
 
 
 def test_dark_theme_by_time_and_user_choice():
     """
-    Протестируйте правильность переключения темной темы на сайте
-    в зависимости от времени и выбора пользователя
+    Протестируйте правильность переключения темной темы на сайте в зависимости от времени и выбора пользователя
     dark_theme_enabled_by_user = True - Темная тема включена
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
     current_time = time(hour=16)
     dark_theme_enabled_by_user = True
-    # TODO переключите темную тему в зависимости от времени суток,
-    #  но учтите что темная тема может быть включена вручную
+    # TODO переключите темную тему в зависимости от времени суток, но учтите что темная тема может быть включена вручную
+    is_night = 22 <= current_time.hour or current_time.hour < 6
+    if dark_theme_enabled_by_user is not None:
+        should_use_dark_theme = dark_theme_enabled_by_user
+    else:
+        should_use_dark_theme = is_night
 
-    is_dark_theme = None
+    is_dark_theme = should_use_dark_theme
     assert is_dark_theme is True
 
 
@@ -42,11 +50,13 @@ def test_find_suitable_user():
     ]
 
     # TODO найдите пользователя с именем "Olga"
-    suitable_users = None
+    user = next((u for u in users if u["name"] == "Olga"), None)
+    suitable_users = user
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    filtered_users = list(filter(lambda u: u["age"] < 20, users))
+    suitable_users = filtered_users
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
@@ -71,15 +81,21 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    readable_name = open_browser.__name__.replace('_', ' ').title()
+    args_str = f"[{browser_name}]"
+    actual_result = f"{readable_name} {args_str}"
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    readable_name = go_to_companyname_homepage.__name__.replace('_', ' ').title()
+    args_str = f"[{page_url}]"
+    actual_result = f"{readable_name} {args_str}"
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    readable_name = find_registration_button_on_login_page.__name__.replace('_', ' ').title()
+    args_str = f"[{page_url}, {button_text}]"
+    actual_result = f"{readable_name} {args_str}"
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
